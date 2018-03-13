4d52b784ce57a56f4c84bb4b354913456eb55c37
amanda.pires <amanda.pires@mac-17.localdomain>
2018-03-13 12:32:08 -0300

    Testing 10...

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,8 +1,8 @@
 stage 'Load files from GitHub'
 def environment, helloworld
 fileLoader.withGit('https://github.com/jenkinsci/workflow-remote-loader-plugin.git', 'master', null, '') {
-    helloworld = fileLoader.load('examples/fileLoader/helloworld');
-    environment = fileLoader.load('examples/fileLoader/environment');
+    helloworld = fileLoader.load('examples/fileLoader/helloworld.groovy');
+    environment = fileLoader.load('examples/fileLoader/environment.groovy');
 }
 
 stage 'Run methods from the loaded content'
