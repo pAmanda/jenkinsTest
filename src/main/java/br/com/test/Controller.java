e7c71a0fae585fa750cb01d106cd4dc5bdb2ce57
pAmanda <amandoca_779@hotmail.com>
2018-03-16 10:58:56 -0300

    Update Controller.java

--- src/main/java/br/com/test/Controller.java
+++ src/main/java/br/com/test/Controller.java
@@ -1,1 +1,13 @@
+package br.com.test;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+  @GetMapping("/")
+  public String webApp() {
+      return "Hello Word!!!";
+  }
+}
